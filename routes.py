1b321e59b59f992df5643f877d4fc297bf13b6f1
Elise Deitrick <edeitrick@codio.com>
2021-07-10 22:21:17 +0000

    dynamically loading captions

--- routes.py
+++ routes.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template, url_for, flash, redirect, Response
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm
-from os import path
+from analyze_audio import printWAV, getDuration
+import time, random, threading
+from turbo_flask import Turbo
 
 app = Flask(__name__)
+interval=10
+FILE_NAME = "Becoming_Preface_shortened.wav"
+turbo = Turbo(app)
+
 app.config['SECRET_KEY'] = 'a01ac6b99ff3af18854f2ee53b064b4b'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
@@ -17,6 +23,37 @@ class User(db.Model):
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
 
+@app.before_first_request
+def before_first_request():
+    #resetting time stamp file to 0
+    file = open("pos.txt","w") 
+    file.write(str(0))
+    file.close()
+    #starting thread that will time updates
+    threading.Thread(target=update_captions).start()
+    
+@app.context_processor
+def inject_load():
+    # getting previous time stamp
+    file = open("pos.txt","r")
+    pos = int(file.read())
+    file.close()
+    print(pos)
+    # writing next time stamp
+    file = open("pos.txt","w")
+    file.write(str(pos+interval))
+    file.close()
+    #returning captions
+    return {'caption':printWAV(FILE_NAME, pos=pos, clip=interval)}
+    
+def update_captions():
+    with app.app_context():
+        while True:
+            # timing thread waiting for the interval
+            time.sleep(interval)
+            # forcefully updating captionsPane with caption
+            turbo.push(turbo.replace(render_template('captionsPane.html'), 'load'))
+    
 @app.route("/")
 @app.route("/home")
 def home():
@@ -39,15 +76,8 @@ def register():
 
 @app.route("/captions")
 def captions():
-    FILE_NAME = "Becoming_Preface_shortened.wav"
-    AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), FILE_NAME)
-    def generate():
-            with open(AUDIO_FILE, "rb") as fwav:
-                data = fwav.read(1024)
-                while data:
-                    yield data
-                    data = fwav.read(1024)
-    return Response(generate(), mimetype="audio/x-wav")
+    TITLE = "Becoming"
+    return render_template('captions.html', songName=TITLE, file=FILE_NAME)
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
